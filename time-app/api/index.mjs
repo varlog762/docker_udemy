3081265cec08f22c0c54f317c8570079c2e7b6fa
Gregory Pereverzev <boot762@gmail.com>
2024-03-06 18:07:19 +0300

    feat: add prettierrc.json

--- time-app/api/index.mjs
+++ time-app/api/index.mjs
@@ -1,34 +1,34 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import {
   readRecords,
   insertRecord,
   deleteRecord,
-} from './src/utils/records.mjs'
+} from './src/utils/records.mjs';
 
-const PORT = 5000
+const PORT = 5000;
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (_, res) => {
-  res.send('Hello from the time saving service!')
-})
+  res.send('Hello from the time saving service!');
+});
 
 app.get('/times', async (_, res) => {
-  res.send(await readRecords())
-})
+  res.send(await readRecords());
+});
 
 app.post('/times', async (req, res) => {
-  res.send(await insertRecord(req.body.time))
-})
+  res.send(await insertRecord(req.body.time));
+});
 
 app.delete('/time/:id', async (req, res) => {
-  res.send(await deleteRecord(req.params.id))
-})
+  res.send(await deleteRecord(req.params.id));
+});
 
 app.listen(PORT, () => {
-  console.log(`Express web server is running at http://localhost:${PORT}`)
-})
+  console.log(`Express web server is running at http://localhost:${PORT}`);
+});
